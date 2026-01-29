187b88f7ee93c416bc3a1631457e14e611f5585c
youhei usami <usao9110@icloud.com>
2026-01-29 12:55:24 +0400

    1/29

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,32 @@ import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
 import BoardList from "./pages/BoardList";
 import Board from "./pages/Board";
+import { currentUserAtom } from "./modules/auth/current-user.state";
+import { useSetAtom } from "jotai";
+import { useEffect, useState } from "react";
+import { authRepository } from "./modules/auth/auth.repository";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const setCurrentUser = useSetAtom(currentUserAtom);
+
+  useEffect(() => {
+    fetchCurrentUser();
+  }, []);
+
+  const fetchCurrentUser = async () => {
+    try {
+      const user = await authRepository.getCurrentUser();
+      setCurrentUser(user);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  if (isLoading) return <div />;
+
   return (
     <BrowserRouter>
       <Routes>
