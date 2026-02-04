e9dd5852e848278c7cf8e1f36e5b50b9c7018b6c
youhei usami <usao9110@icloud.com>
2026-02-04 10:01:22 +0400

    2/4

--- src/components/DraggableObject/index.tsx
+++ src/components/DraggableObject/index.tsx
@@ -1,20 +1,29 @@
-import './DraggableObject.css';
+import "./DraggableObject.css";
 
 interface DraggableObjectProps {
+  x: number;
+  y: number;
   children: React.ReactNode;
   style?: React.CSSProperties;
   className?: string;
 }
 
 export default function DraggableObject({
+  x,
+  y,
   children,
   style,
   className,
 }: DraggableObjectProps) {
+  const containerStyle = {
+    left: x,
+    top: y,
+    ...style,
+  };
   return (
     <div
-      style={style}
-      className={`draggable-object-container ${className || ''}`}
+      style={containerStyle}
+      className={`draggable-object-container ${className || ""}`}
     >
       {children}
     </div>
